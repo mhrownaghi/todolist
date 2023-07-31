364610447aeb0f419356611a511aa130168bcff5
Mohammad Hosein Ronaghi <mhrownaghi@gmail.com>
2023-07-31 17:18:59 +0330

    redirect '/' to '/tasks'

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'this is a test');
+Route::get('/', fn () => redirect('/tasks'));
 
 Route::name('tasks.')->group(function () {
     $controller = TaskController::class;
